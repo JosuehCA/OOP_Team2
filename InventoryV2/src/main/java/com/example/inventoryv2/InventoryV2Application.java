a9b24074b5bb12bb1616ac043b7e8b20c83349a3
JosuehCA <josuemago2012@gmail.com>
2023-11-15 19:27:14 -0600

    SpringBoot initialized

---
+++ InventoryV2/src/main/java/com/example/inventoryv2/InventoryV2Application.java
@@ -0,0 +1,13 @@
+package com.example.inventoryv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryV2Application.class, args);
+	}
+		
+}
---
+++ InventoryV2/src/test/java/com/example/inventoryv2/InventoryV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventoryv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
