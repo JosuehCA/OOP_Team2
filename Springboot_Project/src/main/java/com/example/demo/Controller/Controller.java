4bc3c07b91e25c4550aa19ca755a0e74971572c2
RicardoCv0 <a19203686@alumnos.uady.mx>
2023-11-26 21:32:48 -0600

    Springboot SeafoodTrace
    Se añade el proyecto de springboot

---
+++ Springboot_Project/src/main/java/com/example/demo/Controller/Controller.java
@@ -0,0 +1,56 @@
+package com.example.demo.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.Interface.ProductInterface;
+import com.example.demo.Model.Product;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+@RequestMapping()
+public class Controller {
+   
+   @Autowired
+   private ProductInterface service;
+
+   @GetMapping("/")
+   public String listar(Model model){
+      List<Product>productos=service.listar();
+      model.addAttribute("products", productos);
+      return "home";
+   }
+
+   @GetMapping("/new")
+   public String agregar(Model model){
+      model.addAttribute("Products", new Product());
+      return "form";
+   }
+
+   @PostMapping("/save")
+   public String save(@Validated Product p,Model model){
+      service.save(p);
+      return "redirect:/";
+   }
+
+   @GetMapping("/editar/{id}")
+   public String editar(@PathVariable int id, Model model){
+      Optional<Product>producto=service.listarID(id);
+      model.addAttribute("Products", producto);
+      return "form";
+   }
+
+   @GetMapping("/eliminar/{id}")
+   public String delete(@PathVariable int id, Model model){
+      service.delete(id);
+      return "redirect:/";
+   }
+}
---
+++ Springboot_Project/src/main/java/com/example/demo/Service/ProductService.java
@@ -0,0 +1,45 @@
+package com.example.demo.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.Interface.ProductInterface;
+import com.example.demo.Model.Product;
+import com.example.demo.Repository.ProductsRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService implements ProductInterface {
+   
+   @Autowired
+   private ProductsRepository dato;
+
+   @Override
+   public List<Product> listar() {
+      return (List<Product>) dato.findAll();
+   }
+
+   @Override
+   public Optional<Product> listarID(int id) {
+      return dato.findById(id);
+   }
+
+   @Override
+   public int save(Product p) {
+      int respuesta=0;
+      Product product=dato.save(p);
+      if(!product. equals(null)){
+         respuesta=1;
+      }
+      return respuesta;
+   }
+
+   @Override
+   public void delete(int id) {
+      dato.deleteById(id);
+   }
+
+
+}
